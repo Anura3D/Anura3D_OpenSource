64a302cc6f2f6237951fd3107ea5430a4988dd0c
AlsardiVirginiaTech <alsardi@vt.edu>
2022-07-27 15:52:43 -0600

    Quadratic (debug)

--- src/VS/Debug/deallocationerror__genmod.f90
+++ src/VS/Debug/deallocationerror__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Jul 27 15:49:42 2022
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Jul 27 15:52:02 2022
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE DEALLOCATIONERROR__genmod
