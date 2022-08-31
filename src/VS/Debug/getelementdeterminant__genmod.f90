e65f29370ac33e4461c6a36d6152eba60c896ada
AlsardiVirginiaTech <alsardi@vt.edu>
2022-08-31 17:21:18 -0400

    2D P-wave pulse example works

--- src/VS/Debug/getelementdeterminant__genmod.f90
+++ src/VS/Debug/getelementdeterminant__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Aug 29 16:30:54 2022
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Aug 31 15:27:53 2022
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE GETELEMENTDETERMINANT__genmod
--- src/VS/Debug/load3d__genmod.f90
+++ src/VS/Debug/load3d__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Aug 29 16:30:53 2022
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Aug 31 15:48:52 2022
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE LOAD3D__genmod
