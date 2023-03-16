5512b897da89ea68e2107483d99609f0267615e2
AlsardiVirginiaTech <alsardi@vt.edu>
2023-03-16 18:04:26 -0400

    Commented elastic beam implementation. It is in the beginning of the convective phase

--- src/VS/Debug/initialisetractionload__genmod.f90
+++ src/VS/Debug/initialisetractionload__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Mar 16 09:54:47 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Mar 16 16:41:24 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE INITIALISETRACTIONLOAD__genmod
