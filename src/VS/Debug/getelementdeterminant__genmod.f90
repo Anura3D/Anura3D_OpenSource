dd182ce9b432e368568ab4bfffe270253ecb5c46
AlsardiVirginiaTech <alsardi@vt.edu>
2023-08-03 09:47:15 -0600

    Multipatch: ActiveElement retruned to rank 1

--- src/VS/Debug/getelementdeterminant__genmod.f90
+++ src/VS/Debug/getelementdeterminant__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug  3 08:25:00 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug  3 09:28:18 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE GETELEMENTDETERMINANT__genmod
