8daafef6f9454006d29333776e7da1d46dbef4be
AlsardiVirginiaTech <alsardi@vt.edu>
2023-08-06 11:38:24 -0600

    Multipatch: IsElementCornerNode (rank 2)

--- src/VS/Debug/deallocationerror__genmod.f90
+++ src/VS/Debug/deallocationerror__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Sun Aug  6 11:28:03 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Sun Aug  6 11:34:28 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE DEALLOCATIONERROR__genmod
--- src/VS/Debug/rotvec__genmod.f90
+++ src/VS/Debug/rotvec__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Sun Aug  6 11:27:39 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Sun Aug  6 11:31:47 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE ROTVEC__genmod
