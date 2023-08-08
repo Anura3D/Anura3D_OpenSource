f6cf3adc5b9818bb506f5c60b2d75f93bd2b0b2a
AlsardiVirginiaTech <alsardi@vt.edu>
2023-08-08 13:31:27 -0600

    Multipatch: NSolidEle (rank 2 for multipatch)

--- src/VS/Debug/load2d__genmod.f90
+++ src/VS/Debug/load2d__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Aug  8 11:51:21 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Aug  8 13:30:35 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE LOAD2D__genmod
