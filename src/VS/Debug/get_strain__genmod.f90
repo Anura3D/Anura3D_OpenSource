dda497cfe2d03ba332c75ab4f54d4c63630ea6d2
AlsardiVirginiaTech <alsardi@vt.edu>
2023-08-10 14:00:03 -0600

    Multipatch: SortNodeElementAdjacencies
    do IPatch = 1, NumberOfPatches
              do INode = 1, (NXiKnotEntriesUNIQUE(IPatch)*NEtaKnotEntriesUNIQUE(IPatch)*NZetaKnotEntriesUNIQUE(IPatch))!Counters%NodTot ! loop over all element nodes of the system
                !call IHpSort(NodeElementAdjacencies( NodeElementOffsets(INode)),  ElementsPerNode(INode) )
                !HARDCODED to remove the below subroutine
                call IHpSort(NodeElementAdjacencies( NodeElementOffsets(INode, IPatch) + 1, IPatch),  ElementsPerNode(INode,IPatch) ) ! I removed the + 1 but let's see how this will work out...
    
              end do
              end do

--- src/VS/Debug/get_strain__genmod.f90
+++ src/VS/Debug/get_strain__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug 10 13:38:08 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug 10 13:58:51 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE GET_STRAIN__genmod
--- src/VS/Debug/setactiveelementmock__genmod.f90
+++ src/VS/Debug/setactiveelementmock__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug 10 13:37:53 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug 10 13:58:35 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE SETACTIVEELEMENTMOCK__genmod
