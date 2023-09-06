7891ca80c4525e1c4e72cf7bee36236ebc1c984e
AlsardiVirginiaTech <alsardi@vt.edu>
2023-09-06 15:29:57 -0400

    element tolerances increased to 1d-1 in ElemCalc.FOR

--- src/VS/Debug/initialisetractionloadvector__genmod.f90
+++ src/VS/Debug/initialisetractionloadvector__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Sep  6 09:48:42 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Sep  6 11:33:43 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE INITIALISETRACTIONLOADVECTOR__genmod
--- src/VS/Debug/kernel__genmod.f90
+++ src/VS/Debug/kernel__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Sep  6 09:48:43 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Sep  6 11:33:41 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE KERNEL__genmod
