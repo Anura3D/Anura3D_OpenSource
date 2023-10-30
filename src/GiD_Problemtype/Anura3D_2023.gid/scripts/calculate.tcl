3fc0acd8f6ebfe8ee64d6c642258a737d7a3c840
John Murphy <47335381+murphy-john@users.noreply.github.com>
2023-10-30 10:47:24 -0500

    Update calculate.tcl to create relative path calculate.bat file
    Removed the full project path and replaced with the relative path to the current folder of the "calculate.bat" file. 
    
    This edit was made to add flexibility. The calculate.bat file doesn't need to be edited if the A3D folder location is moved. 
    
    Tested with GiD 16.0.5.

--- src/GiD_Problemtype/Anura3D_2023.gid/scripts/calculate.tcl
+++ src/GiD_Problemtype/Anura3D_2023.gid/scripts/calculate.tcl
@@ -1,18 +1,15 @@
 #      CALCULATE.BAT file
 #      print data in the .dat calculation file (instead of a classic .bas template)
+#      removed the full project path and replaced with the relative path to the current folder of the "calculate.bat" file. 
 proc Anura3D::WriteCalculationFile { filename } {
 GiD_WriteCalculationFile init -mode append $filename
 set project_path [GiD_Info Project ModelName]
 set model_name [file tail $project_path]
 set exe_name [GiD_Info Project ProblemType]
-GiD_WriteCalculationFile puts -nonewline "\""
-GiD_WriteCalculationFile puts -nonewline $project_path
-GiD_WriteCalculationFile puts -nonewline ".A3D\\"
+GiD_WriteCalculationFile puts -nonewline "\".\\"
 GiD_WriteCalculationFile puts -nonewline $exe_name
 GiD_WriteCalculationFile puts -nonewline ".exe\" "
-GiD_WriteCalculationFile puts -nonewline "\""
-GiD_WriteCalculationFile puts -nonewline $project_path
-GiD_WriteCalculationFile puts -nonewline ".A3D\\"
+GiD_WriteCalculationFile puts -nonewline "\".\\"
 GiD_WriteCalculationFile puts -nonewline $model_name
 GiD_WriteCalculationFile puts "\""
 GiD_WriteCalculationFile puts "PAUSE"
