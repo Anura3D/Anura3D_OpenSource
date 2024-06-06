2799e581b0dab82598e4c0e4b200a191b3839e3f
AlsardiVirginiaTech <alsardi@vt.edu>
2024-06-06 11:34:20 -0600

    Load3D_NURBS_OnMPs clean up

--- src/VS/Debug/load2d__genmod.f90
+++ src/VS/Debug/load2d__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Jun  6 09:42:34 2024
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Jun  6 11:33:06 2024
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE LOAD2D__genmod
