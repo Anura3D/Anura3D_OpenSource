9eafea8d85b6678c6dcb4451850fc3b2ffe893db
Jonathan Moore <jonathanm@vt.edu>
2024-08-30 20:59:45 -0400

    Updated the calculate.bat file so that it uses the local executable

--- src/GiD_Problemtype/Anura3D_2024.gid/scripts/calculate.tcl
+++ src/GiD_Problemtype/Anura3D_2024.gid/scripts/calculate.tcl
@@ -6,9 +6,16 @@ set project_path [GiD_Info Project ModelName]
 set model_name [file tail $project_path]
 set exe_name [GiD_Info Project ProblemType]
 GiD_WriteCalculationFile puts -nonewline "\""
-GiD_WriteCalculationFile puts -nonewline $project_path
-GiD_WriteCalculationFile puts -nonewline ".A3D\\"
-GiD_WriteCalculationFile puts -nonewline $exe_name
+# GiD_WriteCalculationFile puts -nonewline $project_path
+# GiD_WriteCalculationFile puts -nonewline ".A3D\\"
+
+# Find the position of the last '/' in the string
+set lastSlashIndex [string last / $exe_name]
+
+# Extract the name of the executable after the last '/' symbol
+set only_exe_name [string range $exe_name [expr {$lastSlashIndex + 1}] end]
+
+GiD_WriteCalculationFile puts -nonewline $only_exe_name
 GiD_WriteCalculationFile puts -nonewline ".exe\" "
 GiD_WriteCalculationFile puts -nonewline "\""
 GiD_WriteCalculationFile puts -nonewline $project_path
